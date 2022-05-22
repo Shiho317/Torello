bffc533566e79dcaefb331e72a017fe78f1fc373
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-22 12:13:05 -0700

    added signup data func

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { FormWrapper, LoginForm, LoginWrapper } from './Login.style'
 
 const Login = () => {
+
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const navigate = useNavigate()
+
+  const loginAccount = async(e) => {
+    e.preventDefault()
+
+    const currUser = {
+      email: emailRef,
+      password: passwordRef
+    }
+
+    try {
+      const result = await axios.get("http://localhost:8888", currUser)
+      const userData = {
+        name: result.data.name,
+        email: result.data.email,
+        password: result.data.password,
+        id: result.data._id
+      }
+      
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <LoginWrapper>
       <FormWrapper>
         <h1>Login</h1>
-      <LoginForm>
-        <input type='email' placeholder="Email" />
-        <input type='password' placeholder="Password" />
+      <LoginForm onSubmit={(e) => loginAccount(e)}>
+        <input type='email' placeholder="Email" ref={emailRef} />
+        <input type='password' placeholder="Password" ref={passwordRef} />
         <button type='submit'>Log in</button>
       </LoginForm>
         <Link to='/signup'>
--- client/src/components/account/Signup.jsx
+++ client/src/components/account/Signup.jsx
@@ -1,16 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { SignupForm, SignupWrapper, SUformWrap } from "./Signup.style";
+import axios from 'axios'
 
 const Signup = () => {
+
+  const usernameRef = useRef(null)
+  const useremailRef = useRef(null)
+  const userpasswordRef = useRef(null)
+
+  const navigate = useNavigate()
+
+  const addAccount = async(e) => {
+    e.prevendDefault();
+
+    const newUser = {
+      name: usernameRef,
+      email: useremailRef,
+      password: userpasswordRef
+    }
+    try {
+      await axios.post("http://localhost:8888", newUser)
+      alert("You have successfully created account.");
+      setTimeout(() => {
+        navigate('/')
+      }, 500)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <SignupWrapper>
       <SUformWrap>
         <h1>SignUp</h1>
-        <SignupForm>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <SignupForm onSubmit={(e) => addAccount(e)}>
+          <input type="text" placeholder="Name" ref={usernameRef} />
+          <input type="email" placeholder="Email" ref={useremailRef} />
+          <input type="password" placeholder="Password" ref={userpasswordRef} />
           <button type="submit">Log in</button>
         </SignupForm>
         <Link to="/">
