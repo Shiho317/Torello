84cef875bf45c41002b8d8e37464f6e4d82128f1
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-25 09:40:14 -0700

    added deploy setting

--- server.js
+++ server.js
@@ -31,7 +31,15 @@ mongoose
 app.use(cors());
 app.use("/api/user", userRoute)
 app.use("/api/list", listRoute)
+app.use(express.static("public"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8888;
 
